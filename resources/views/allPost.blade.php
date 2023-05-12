188e99c0e69ec87a802061e9ee9bce7a4b5b9279
anagararna <anagararna@gmail.com>
2023-05-12 20:45:03 +0200

    add title

--- resources/views/allPost.blade.php
+++ resources/views/allPost.blade.php
@@ -2,7 +2,7 @@
 <!-- component -->
 <section class="bg-white dark:bg-gray-900">
     <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">El Rincón de isma Blog</h1>
+        <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">Blog</h1>
 
         <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
             @foreach ($posts as $post)
@@ -13,7 +13,7 @@
 
                 <div class="flex flex-col justify-between py-6 lg:mx-6">
                     <a href="{{ route('posts.view',$post->id) }}" class="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
-                        {{ $post->title }}
+                        <h1>{{ $post->title }}</h1>
                     </a>
                     
                     <span class="text-sm text-gray-500 dark:text-gray-300">En: {{ \Carbon\Carbon::parse($post->created_at)->format('l jS \\of F Y h:i:s A') }}</span>
